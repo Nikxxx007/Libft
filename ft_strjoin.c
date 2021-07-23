e1fbdc70db92f3ccb85c2584bae7b536997305fd
Rosalinda Gaynell <rogaynel@am-h1.msk.21-school.ru>
2021-07-23 14:44:38 +0300

    func fix

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -2,28 +2,19 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*result;
-	size_t	i;
-	size_t	j;
+	char    *result;
+    size_t    s1_len;
+    size_t    s2_len;
 
-	if (!s1 || !s2)
-		return (NULL);
-	i = 0;
-	j = 0;
-	result = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
-	if (!result)
-		return (NULL);
-	while (i < ft_strlen(s1))
-	{
-		result[i] = s1[i];
-		i++;
-	}
-	while (j < ft_strlen(s2))
-	{
-		result[i] = s2[j];
-		j++;
-		i++;
-	}
-	result[i] = '\0';
-	return (result);
+    if (!s1 && !s2)
+        return (0);
+    s1_len = ft_strlen((char *)s1);
+    s2_len = ft_strlen((char *)s2);
+    result = malloc(sizeof(char) * (s1_len + s2_len) + 1);
+    if (!result)
+        return (0);
+    ft_memmove(result, s1, s1_len);
+    ft_memmove(result + s1_len, s2, s2_len);
+    result [s1_len + s2_len] = '\0';
+    return (result);
 }
